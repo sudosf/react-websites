82c93a18280ce19d9a1c038912d16c5475816bb6
sudosf <sudosprogrammer@gmail.com>
2023-09-12 04:29:01 +0200

    first props test succeeded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import r1Img from "../assets/r1.jpg"; 
 
-export default function Card() {
+interface Props {
+    img: string;
+    name: string;
+    tel: string;
+    email: string;
+
+}
+
+export default function Card(props: Props) {
    
     return (
-        <section className="card mx-auto my-2" style={{'width': '18rem'}}>
-        <img src={r1Img} className="img-fluid w-100 p-2 shadow rounded" alt="grid-image" />
+        <section className="card mx-auto my-2 shadow" style={{'width': '18rem'}}>
+        <img src={props.img} className="img-fluid w-100 shadow rounded-top" alt="grid-image" />
         <div className="card-body">
             
-            <h1 className="card-title">Jerry Docks</h1>
+            <h1 className="card-title">{props.name}</h1>
             <p className="card-text">
                 <i className="fa fa-solid fa-phone"></i>
-                &nbsp; (+27) 72 050 3491
+                &nbsp; {props.tel}
             </p>
 
             <p className="card-text">
                 <i className="fa-solid fa-envelope"></i>
-                &nbsp; jerry.docks@mouse.cheese         
+                &nbsp; {props.email}         
             </p>
         </div>
     </section>
     );
 
-}
+} 

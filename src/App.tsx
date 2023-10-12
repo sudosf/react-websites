ce6d368cc1d87874dc3622f2882f892806916cc3
sudosf <sudosprogrammer@gmail.com>
2023-10-12 03:57:02 +0200

    testing new ssh key

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import "./App.css";
 
 /* Components */
 import Navbar  from "./components/Navbar2";
-import Form from "./components/Form";
 import Meme from "./components/Meme";
-import Count from "./components/Count";
 
 export default function App() {
 
 	return (
 	<>
 	
-	< Navbar />
-	< Meme />
+		< Navbar />
+		< Meme />
 
 	</>
 	);

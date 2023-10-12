39c714d3fde72d2ff0455139d6432b594ace4600
sudosf <sudosprogrammer@gmail.com>
2023-10-12 07:15:24 +0200

    vs code tutorial

---
+++ src/vs code/test.js
@@ -0,0 +1,22 @@
+
+const person = {
+    age: 10,
+    name: welcome
+};
+
+
+/**
+ * 
+ * @param {string} message
+ * 
+ */
+
+function sayAlert(message) {
+    alert(message);     
+}
+
+sayAlert("Hellos, bees");
+sayAlert("Hellos, bees");
+sayAlert("Hellos, bees");
+sayAlert("Hellos, bee");
+

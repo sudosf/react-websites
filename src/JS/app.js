70de354d164d0f93a6e37901ab1cc20560a253de
sudosf <sudosprogrammer@gmail.com>
2023-10-13 07:50:42 +0200

    Easy imports, Js Booster, Eslint, path intelisense

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,9 +1,15 @@
 import add from "./helpers/add";
-
+import React from 'react';
+import Add from './helpers/add';
 add(1, 2);
 
 // const message = "i do not change";
 let message = "i do not change";
 
 message = "No, i do";
+console.log("🚀 ~ file: app.js:10 ~ message:", message)
+
+let change = () => {
+    console.log("object");
+};
 

7dd4abfe8fd6c1c40d05eb51ce4e682218dc426a
sudosf <sudosprogrammer@gmail.com>
2023-10-20 17:39:51 +0200

    prettier formatting edits

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,3 @@
-
 interface Data {
     img: string;
     name: string;
@@ -8,39 +7,45 @@ interface Data {
 }
 
 export default function Card(props: Data) {
-   
     const nums: number[] = [1, 2, 4, 3, 5];
     const names: string[] = ["a", "bob", "eli"];
 
-    const testOne = nums.map(function(item: number) {
+    const testOne = nums.map(function (item: number) {
         return item * item;
     });
 
-    const testTwo = names.map(function(name: string) {
+    const testTwo = names.map(function (name: string) {
         return name[0].toUpperCase() + name.slice(1);
     });
 
     // console.log(props);
 
     return (
-        <section className="card mx-auto my-2 shadow" style={{'width': '18rem'}}>
-        <img src={props.img} className="img-fluid w-100 h-100 shadow rounded-top" alt="grid-image" />
-        <div className="card-body">
-            
-            <h1 className="card-title">{props.name}</h1> 
+        <section
+            className="card mx-auto my-2 shadow"
+            style={{ width: "18rem" }}
+        >
+            <img
+                src={props.img}
+                className="img-fluid w-100 h-100 shadow rounded-top"
+                alt="grid-image"
+            />
+            <div className="card-body">
+                <h1 className="card-title">{props.name}</h1>
 
-            <p className="card-text" style={{'color': props.valid ? 'green' : 'red'}}
-            >
-                <i className="fa fa-solid fa-phone"></i>
-                &nbsp; {props.tel}
-            </p>
+                <p
+                    className="card-text"
+                    style={{ color: props.valid ? "green" : "red" }}
+                >
+                    <i className="fa fa-solid fa-phone"></i>
+                    &nbsp; {props.tel}
+                </p>
 
-            <p className="card-text">
-                <i className="fa-solid fa-envelope"></i>
-                &nbsp; {props.email}         
-            </p>
-        </div>
-    </section>
+                <p className="card-text">
+                    <i className="fa-solid fa-envelope"></i>
+                    &nbsp; {props.email}
+                </p>
+            </div>
+        </section>
     );
-
-} 
+}
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,89 +1,89 @@
 import React from "react";
 
 export default function Meme() {
-  const [meme, setMeme] = React.useState({
-    topText: "",
-    bottomText: "",
-    image: "http://i.imgflip.com/1bij.jpg",
-  });
-
-  const [allMemes, setAllMemes] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((result) => setAllMemes(result.data.memes));
-  }, []);
-
-  function handleChange(event) {
-    setMeme((prevMeme) => {
-      const { name, value } = event.target;
-      return {
-        ...prevMeme,
-        [name]: value,
-      };
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        image: "http://i.imgflip.com/1bij.jpg",
     });
-  }
 
-  function getMemeImage() {
-    const randomNum = Math.floor(Math.random() * allMemes.length);
-    const url = allMemes[randomNum].url;
+    const [allMemes, setAllMemes] = React.useState([]);
 
-    setMeme((prevMeme) => ({
-      ...prevMeme,
-      image: url,
-    }));
-  }
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then((res) => res.json())
+            .then((result) => setAllMemes(result.data.memes));
+    }, []);
 
-  return (
-    <main className="container my-5 text-white">
-      <div className="text-center mb-3">
-        <p>Enter Meme Text</p>
-      </div>
+    function handleChange(event) {
+        setMeme((prevMeme) => {
+            const { name, value } = event.target;
+            return {
+                ...prevMeme,
+                [name]: value,
+            };
+        });
+    }
 
-      <div className="mb-3 d-flex">
-        <input
-          type="text"
-          className="form-control me-2 border border-secondary"
-          id="topTextInput"
-          placeholder="Top Text"
-          name="topText"
-          value={meme.topText}
-          onChange={handleChange}
-        />
+    function getMemeImage() {
+        const randomNum = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomNum].url;
 
-        <input
-          type="text"
-          className="form-control ms-2 border border-secondary"
-          id="bottomTextInput"
-          placeholder="Bottom Text"
-          name="bottomText"
-          value={meme.bottomText}
-          onChange={handleChange}
-        />
-      </div>
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            image: url,
+        }));
+    }
 
-      <div className="text-center mt-4 d-grid gap-2">
-        <button
-          className="btn btn-success btn-lg btn-block"
-          name="submit"
-          onClick={getMemeImage}
-        >
-          Get a new Meme image
-        </button>
-      </div>
+    return (
+        <main className="container my-5 text-white">
+            <div className="text-center mb-3">
+                <p>Enter Meme Text</p>
+            </div>
 
-      <div className="text-center mt-4 d-grid gap-2">
-        <p>{meme.topText}</p>
-        <img
-          src={meme.image}
-          className="img w-100 h-100 shadow rounded-top"
-          alt="grid-image"
-        />
-        <p>{meme.bottomText}</p>
-      </div>
+            <div className="mb-3 d-flex">
+                <input
+                    type="text"
+                    className="form-control me-2 border border-secondary"
+                    id="topTextInput"
+                    placeholder="Top Text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
 
-      <pre>{JSON.stringify(allMemes, null, 0)}</pre>
-    </main>
-  );
+                <input
+                    type="text"
+                    className="form-control ms-2 border border-secondary"
+                    id="bottomTextInput"
+                    placeholder="Bottom Text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="text-center mt-4 d-grid gap-2">
+                <button
+                    className="btn btn-success btn-lg btn-block"
+                    name="submit"
+                    onClick={getMemeImage}
+                >
+                    Get a new Meme image
+                </button>
+            </div>
+
+            <div className="text-center mt-4 d-grid gap-2">
+                <p>{meme.topText}</p>
+                <img
+                    src={meme.image}
+                    className="img w-100 h-100 shadow rounded-top"
+                    alt="grid-image"
+                />
+                <p>{meme.bottomText}</p>
+            </div>
+
+            <pre>{JSON.stringify(allMemes, null, 0)}</pre>
+        </main>
+    );
 }
